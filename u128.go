96c5d6ff44d4f88fc61d12887491c07eb7b16e85
Mocha Dcaf <mocha@dcaf.so>
2023-03-09 23:27:09 -0500

    feat: implement scanner and valuer interface for u128, i128

--- u128.go
+++ u128.go
@@ -18,6 +18,7 @@
 package bin
 
 import (
+	"database/sql/driver"
 	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
@@ -31,6 +32,60 @@ type Uint128 struct {
 	Lo         uint64
 	Hi         uint64
 	Endianness binary.ByteOrder
+}
+
+func (p *Uint128) FromBigInt(value *big.Int) error {
+	if value.Sign() < 0 {
+		return fmt.Errorf("negative value cannot be assigned to uint128")
+	}
+	if len(value.Bytes()) > 16 {
+		return fmt.Errorf("val contains %d extra bytes for int128", 16-len(value.Bytes()))
+	}
+	bytes := value.FillBytes(make([]byte, 16))
+	p.Lo = binary.BigEndian.Uint64(bytes[8:])
+	p.Hi = binary.BigEndian.Uint64(bytes[:8])
+	p.Endianness = binary.BigEndian
+	return nil
+}
+
+// Scan assigns a value from a database driver.
+func (p *Uint128) Scan(value any) error {
+	if value == nil {
+		return nil
+	}
+	switch s := value.(type) {
+	case string:
+		val, ok := new(big.Int).SetString(s, 10)
+		if !ok {
+			return fmt.Errorf("failed to convert string to bigint when scanning Int128")
+		}
+		if val.Sign() < 0 {
+			return fmt.Errorf("negative value cannot be assigned to uint128")
+		}
+		bytes := val.FillBytes(make([]byte, 16))
+		return p.Scan(bytes)
+	case int64:
+		bytes := make([]byte, 16)
+		binary.BigEndian.PutUint64(bytes, uint64(s))
+		return p.Scan(bytes)
+	case []byte:
+		if len(s) > 16 {
+			return fmt.Errorf("failed to scan Int128, value larger than 16 bytes")
+		}
+		p.Lo = binary.BigEndian.Uint64(s[8:])
+		p.Hi = binary.BigEndian.Uint64(s[:8])
+		p.Endianness = binary.BigEndian
+		return nil
+	}
+	return fmt.Errorf("unsupported Scan, storing driver.Value type %T into type %T", value, p)
+}
+
+// Value returns a driver Value.
+func (p *Uint128) Value() (driver.Value, error) {
+	if p == nil {
+		return nil, nil
+	}
+	return p.String(), nil
 }
 
 func NewUint128BigEndian() *Uint128 {
@@ -191,6 +246,67 @@ func (i Uint128) MarshalWithEncoder(enc *Encoder) error {
 
 // Int128
 type Int128 Uint128
+
+func (p *Int128) FromBigInt(value *big.Int) error {
+	if len(value.Bytes()) > 16 {
+		return fmt.Errorf("val contains %d extra bytes for int128", 16-len(value.Bytes()))
+	}
+	bytes := value.FillBytes(make([]byte, 16))
+	p.Lo = binary.BigEndian.Uint64(bytes[8:])
+	p.Hi = binary.BigEndian.Uint64(bytes[:8])
+	p.Endianness = binary.BigEndian
+	// https://stackoverflow.com/questions/74709367/converting-big-int-to-2int64-vice-versa-and-twos-complement
+	if value.Sign() < 0 {
+		// convert the unsigned value to two's compliment
+		p.Lo = ^p.Lo
+		p.Hi = ^p.Hi
+
+		p.Lo++
+		// check for carry
+		if p.Lo == 0 {
+			p.Hi++
+		}
+	}
+	return nil
+}
+
+// Scan assigns a value from a database driver.
+func (p *Int128) Scan(value any) error {
+	if value == nil {
+		return nil
+	}
+	switch s := value.(type) {
+	case string:
+		val, ok := new(big.Int).SetString(s, 10)
+		if !ok {
+			return fmt.Errorf("failed to convert string to bigint when scanning Int128")
+		}
+		bytes := val.FillBytes(make([]byte, 16))
+		return p.Scan(bytes)
+	case int64:
+		bytes := make([]byte, 16)
+		binary.BigEndian.PutUint64(bytes, uint64(s))
+		return p.Scan(bytes)
+	case []byte:
+		if len(s) > 16 {
+			return fmt.Errorf("failed to scan Int128, value larger than 16 bytes")
+		}
+		p.Lo = binary.BigEndian.Uint64(s[8:])
+		p.Hi = binary.BigEndian.Uint64(s[:8])
+		p.Endianness = binary.BigEndian
+
+		return nil
+	}
+	return fmt.Errorf("unsupported Scan, storing driver.Value type %T into type %T", value, p)
+}
+
+// Value returns a driver Value.
+func (p *Int128) Value() (driver.Value, error) {
+	if p == nil {
+		return nil, nil
+	}
+	return p.String(), nil
+}
 
 func (i Int128) BigInt() *big.Int {
 	comp := byte(0x80)
